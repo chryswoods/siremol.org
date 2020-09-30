274593fe9a15e76f87a49c54e7b6fd4ebaf7705e
Christopher Woods <chryswoods@hey.com>
2020-09-30 15:28:45 +0100

    Updates to Beginning R and now starting work on Intermediate R

---
+++ chryswoods.com/intermediate_r/render_all.sh
@@ -0,0 +1,7 @@
+R -e "rmarkdown::render('index.Rmd',output_file='index.html')"
+R -e "rmarkdown::render('formatting.Rmd',output_file='formatting.html')"
+R -e "rmarkdown::render('packages.Rmd',output_file='packages.html')"
+R -e "rmarkdown::render('functions.Rmd',output_file='functions.html')"
+R -e "rmarkdown::render('tidyverse.Rmd',output_file='tidyverse.html')"
+R -e "rmarkdown::render('magrittr.Rmd',output_file='magrittr.html')"
+R -e "rmarkdown::render('summary.Rmd',output_file='summary.html')"
