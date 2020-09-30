8666244a674c4453680177b717ade5e700f38796
Christopher Woods <chryswoods@hey.com>
2020-09-30 16:58:51 +0100

    Adding the packages page, completed the formatting page

--- chryswoods.com/intermediate_r/render_all.sh
+++ chryswoods.com/intermediate_r/render_all.sh
@@ -1,5 +1,7 @@
 R -e "rmarkdown::render('index.Rmd',output_file='index.html')"
 R -e "rmarkdown::render('formatting.Rmd',output_file='formatting.html')"
+R -e "rmarkdown::render('formatting_answer01.Rmd',output_file='formatting_answer01.html')"
+R -e "rmarkdown::render('formatting_answer02.Rmd',output_file='formatting_answer02.html')"
 R -e "rmarkdown::render('packages.Rmd',output_file='packages.html')"
 R -e "rmarkdown::render('functions.Rmd',output_file='functions.html')"
 R -e "rmarkdown::render('tidyverse.Rmd',output_file='tidyverse.html')"
