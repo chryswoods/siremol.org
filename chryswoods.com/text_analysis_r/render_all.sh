cae16d894a6d718e217463bcc1b84db4947769ab
Christopher Woods <chryswoods@hey.com>
2021-04-16 17:45:14 +0100

    Making progress with the content of the workshop

---
+++ chryswoods.com/text_analysis_r/render_all.sh
@@ -0,0 +1,8 @@
+R -e "rmarkdown::render('index.Rmd',output_file='index.html')"
+R -e "rmarkdown::render('tokenising.Rmd',output_file='tokenising.html')"
+R -e "rmarkdown::render('regexp.Rmd',output_file='regexp.html')"
+R -e "rmarkdown::render('sentiment.Rmd',output_file='sentiment.html')"
+R -e "rmarkdown::render('wordclouds.Rmd',output_file='wordclouds.html')"
+R -e "rmarkdown::render('ngrams.Rmd',output_file='ngrams.html')"
+R -e "rmarkdown::render('correlations.Rmd',output_file='correlations.html')"
+R -e "rmarkdown::render('summary.Rmd',output_file='summary.html')"
