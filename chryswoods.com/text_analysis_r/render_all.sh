642acf5319870a93d8182966f6cd7f611524c781
Christopher Woods <chryswoods@hey.com>
2021-04-19 18:14:14 +0100

    Making progress - now filling in all of the detail of the individual pages. Completed the index and tokenising pages.

--- chryswoods.com/text_analysis_r/render_all.sh
+++ chryswoods.com/text_analysis_r/render_all.sh
@@ -1,5 +1,6 @@
 R -e "rmarkdown::render('index.Rmd',output_file='index.html')"
 R -e "rmarkdown::render('tokenising.Rmd',output_file='tokenising.html')"
+R -e "rmarkdown::render('tokenising_answer.Rmd',output_file='tokenising_answer.html')"
 R -e "rmarkdown::render('regexp.Rmd',output_file='regexp.html')"
 R -e "rmarkdown::render('sentiment.Rmd',output_file='sentiment.html')"
 R -e "rmarkdown::render('wordclouds.Rmd',output_file='wordclouds.html')"
