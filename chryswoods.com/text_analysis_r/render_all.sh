96f9a67594b11f3ee3fd5df9f863d6daa8c171f7
Christopher Woods <chryswoods@hey.com>
2021-04-27 18:21:57 +0100

    Making progress - finished the regexp chapter

--- chryswoods.com/text_analysis_r/render_all.sh
+++ chryswoods.com/text_analysis_r/render_all.sh
@@ -4,6 +4,7 @@ R -e "rmarkdown::render('tokenising_answer.Rmd',output_file='tokenising_answer.h
 R -e "rmarkdown::render('sentiment.Rmd',output_file='sentiment.html')"
 R -e "rmarkdown::render('sentiment_answer.Rmd',output_file='sentiment_answer.html')"
 R -e "rmarkdown::render('regexp.Rmd',output_file='regexp.html')"
+R -e "rmarkdown::render('regexp_answer.Rmd',output_file='regexp_answer.html')"
 R -e "rmarkdown::render('wordclouds.Rmd',output_file='wordclouds.html')"
 R -e "rmarkdown::render('ngrams.Rmd',output_file='ngrams.html')"
 R -e "rmarkdown::render('summary.Rmd',output_file='summary.html')"
