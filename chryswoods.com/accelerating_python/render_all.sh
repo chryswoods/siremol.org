87cb7909af1332678d781fa6fe16075725e3beaf
Christopher Woods <Christopher.Woods@bristol.ac.uk>
2022-02-04 10:59:25 +0000

    Can render the files

---
+++ chryswoods.com/accelerating_python/render_all.sh
@@ -0,0 +1,3 @@
+R -e "rmarkdown::render('index.Rmd',output_file='index.html')"
+R -e "rmarkdown::render('installing.Rmd',output_file='installing.html')"
+R -e "rmarkdown::render('timing.Rmd',output_file='timing.html')"
