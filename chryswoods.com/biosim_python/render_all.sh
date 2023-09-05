be1df00915e03159dbb6feabdb20735e81479d7e
Christopher Woods <Christopher.Woods@bristol.ac.uk>
2023-09-05 11:53:14 +0100

    Added a quick summary python workshop for ccp-biosim

---
+++ chryswoods.com/biosim_python/render_all.sh
@@ -0,0 +1,4 @@
+R -e "rmarkdown::render('index.Rmd',output_file='index.html')"
+R -e "rmarkdown::render('python_zero_sixty.Rmd',output_file='python_zero_sixty.html')"
+R -e "rmarkdown::render('python_data.Rmd',output_file='python_data.html')"
+R -e "rmarkdown::render('python_biosimspace.Rmd',output_file='python_biosimspace.html')"
